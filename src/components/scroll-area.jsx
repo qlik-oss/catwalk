f4cbffa26994ca0f09cb2bee2460499a210eb454
Helene Rigner <hvs@qlik.com>
2018-10-17 15:40:00 +0200

    Making the grid area drag-scrollable

---
+++ src/components/scroll-area.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class ScrollArea extends React.Component {
+  constructor(props) {
+    super(props);
+    this.scrollArea = React.createRef();
+    this.state = {
+      isScrolling: false,
+      scrollAreaStyle: { style: { height: props.height, width: props.width, overflow: 'auto' } },
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('mouseup', this.onMouseUp);
+    window.addEventListener('mousemove', this.onMouseMove);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('mouseup', this.onMouseUp);
+    window.removeEventListener('mousemove', this.onMouseMove);
+  }
+
+  onMouseUp = () => {
+    const { isScrolling, scrollAreaStyle } = this.state;
+    if (isScrolling) {
+      const cursorStyle = {
+        ...scrollAreaStyle.style,
+        cursor: 'unset',
+      };
+      this.setState({ isScrolling: false, scrollAreaStyle: { style: cursorStyle } });
+    }
+  }
+
+  onMouseDown = (event) => {
+    const { isScrolling, scrollAreaStyle } = this.state;
+    if (!isScrolling) {
+      this.lastClientX = event.clientX;
+      this.lastClientY = event.clientY;
+      const cursorStyle = {
+        ...scrollAreaStyle.style,
+        cursor: 'grab',
+      };
+      this.setState({ isScrolling: true, scrollAreaStyle: { style: cursorStyle } });
+      event.preventDefault();
+    }
+  }
+
+  onMouseMove = (event) => {
+    const { isScrolling } = this.state;
+    if (isScrolling) {
+      this.scrollArea.scrollLeft -= (-this.lastClientX + (this.lastClientX = event.clientX));
+      this.scrollArea.scrollTop -= (-this.lastClientY + (this.lastClientY = event.clientY));
+    }
+  }
+
+  renderChildren(dom) {
+    return React.cloneElement(dom, {
+      onMouseUp: this.onMouseUp,
+      onMouseDown: this.onMouseDown,
+    });
+  }
+
+  render() {
+    const { scrollAreaStyle } = this.state;
+    const { className, children } = this.props;
+    return (
+      <div
+        className={className}
+        {...scrollAreaStyle}
+        onMouseUp={this.onMouseUp}
+        onMouseMove={this.onMouseMove}
+        ref={(e) => { this.scrollArea = e; }}
+        role="tabList"
+        tabIndex={-1}
+      >
+        {children && this.renderChildren(children)}
+      </div>
+    );
+  }
+}
+
+ScrollArea.propTypes = {
+  width: PropTypes.string.isRequired,
+  height: PropTypes.string.isRequired,
+};
+
+export default ScrollArea;
