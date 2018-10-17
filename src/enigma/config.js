b840e0007021eed76fc55bbf0c7a3f1a2a81f4fe
Håkan Axelsson <han@qlik.com>
2018-10-17 05:20:51 -0400

    Use url search params (#29)
    * Use URLSearchParams for setting engine url
    * Use enigma.js mixin
    * Adding input field for QAE WS
    * Change session param to engine_url

--- src/enigma/config.js
+++ src/enigma/config.js
@@ -1,15 +1,16 @@
-import schema from 'enigma.js/schemas/12.20.0.json';
+import schema from 'enigma.js/schemas/12.170.2.json';
 
 import listCache from './list-cache';
 import layoutCache from './layout-cache';
+import getDoc from './get-doc';
 
 const ERR_ABORTED = 15;
-const url = process.env.ENGINE_URL;
 
 const config = {
   schema,
-  createSocket: () => new WebSocket(url || `ws://${location.hostname}:9076/app/${+new Date()}`), // eslint-disable-line
-  mixins: [listCache, layoutCache],
+  url: new URLSearchParams(document.location.search).get('engine_url') || `ws://localhost:9076/app/${+new Date()}`,
+  createSocket: url => new WebSocket(url),
+  mixins: [listCache, layoutCache, getDoc],
   responseInterceptors: [{
     onRejected(session, request, error) {
       if (error.code === ERR_ABORTED) {
