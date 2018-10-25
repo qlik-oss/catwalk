14c07b80d626791cf5e8715c362afc1c253ea2c7
Gabriel Weitoft <gabriel.weitoft@qlik.com>
2018-10-25 13:24:57 +0200

    New field and filterbox implementation (#57)
    Fixes #42 
    Fixes #28 
    Fixes #10 
    Fixes #7 
    
    Virtual scrolling in filterbox with full range data paging
    Key type visible as 1..n, 1..1, n..n labels on the link, and not only color shades
    Sublime color indications for zero and one matching values in a field
    Support for data islands
    Initial support for example values in the hover title
    Right aligned frequency column in filterbox

--- src/components/filterbox.jsx
+++ src/components/filterbox.jsx
@@ -1,49 +1,108 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
-import withApp from './with-app';
-import withModel from './with-model';
-import withLayout from './with-layout';
-
-import './filterbox.css';
+import Column from 'react-virtualized/dist/es/Table/Column';
+import VirtualTable from './virtual-table';
+import './filterbox.scss';
 
 const KEY_ENTER = 13;
 
 function preventDefaultFn(event) {
   event.stopPropagation();
 }
+
+function listboxNameColumnGetter({ rowData }) {
+  return rowData ? rowData[0].qText : '...';
+}
+
+
+function nameCellRenderer({ rowData }) {
+  if (!rowData) {
+    return (<div>...</div>);
+  }
+  let title;
+  if (!rowData[0].qNum || rowData[0].qNum === 'NaN') {
+    title = `'${rowData[0].qText || ''}' (No numerical representation)`;
+  } else {
+    title = `'${rowData[0].qText}' (Numerical representation: ${rowData[0].qNum})`;
+  }
+  return (
+    <div title={title}>
+      {rowData[0].qText ? rowData[0].qText : '< empty >'}
+    </div>
+  );
+}
+nameCellRenderer.propTypes = {
+  rowData: PropTypes.object.isRequired,
+};
+
+function listboxFrequencyColumnGetter({ rowData }) {
+  return rowData ? `${rowData[0].qFrequency || '0'}x` : '...';
+}
+
+function freqCellRenderer({ rowData }) {
+  if (!rowData) {
+    return (<div>...</div>);
+  }
+  if (rowData[0].qState === 'X') {
+    return (<div />);
+  }
+
+
+  const freq = rowData[0].qFrequency || 1;
+  const pluralized = freq > 1 ? 'times ' : 'time';
+  return (
+    <div title={`This value occurs ${freq} ${pluralized}`}>
+      {`${freq}x`}
+    </div>
+  );
+}
+freqCellRenderer.propTypes = {
+  rowData: PropTypes.object.isRequired,
+};
+
+function rowRenderer({
+  defaultProps, rowData, style, columns, className, key,
+}) {
+  const classes = `item state-${rowData ? rowData[0].qState : 'Loading'} ${className}`;
+  return (
+    <div
+      {...defaultProps}
+      className={classes}
+      key={key}
+      role="row"
+      style={style}
+    >
+      {columns}
+    </div>
+  );
+}
+
+function noRowsRenderer() {
+  return <div className="no-values">No values</div>;
+}
+
 export class Filterbox extends React.Component {
   constructor() {
     super();
-    this.state = {};
-  }
-
-  componentWillMount() {
-    const { alignTo } = this.props;
-    if (alignTo) {
-      const rect = alignTo.getBoundingClientRect();
-      this.setState({
-        style: {
-          position: 'absolute',
-          top: rect.top + rect.height + window.scrollY,
-          left: rect.left + window.scrollX - 1,
-          width: rect.width + 2, // 1px border x2
-        },
-      });
-    }
+    this.onRowClick = this.onRowClick.bind(this);
+    this.onSearch = this.onSearch.bind(this);
   }
 
   componentWillUnmount() {
-    const { onClose } = this.props;
-    if (onClose) {
-      onClose();
+    const { model } = this.props;
+    if (this.ongoingSearch) {
+      model.abortListObjectSearch('/qListObjectDef');
+    }
+    if (this.searchTimer) {
+      clearTimeout(this.searchTimer);
     }
   }
 
-  search(evt) {
+  onSearch(evt) {
     const { value } = evt.target;
     const { keyCode } = evt;
     const { model } = this.props;
+    this.ongoingSearch = value;
     if (keyCode === KEY_ENTER) {
       model.acceptListObjectSearch('/qListObjectDef', true);
       evt.target.blur();
@@ -56,59 +115,67 @@ export class Filterbox extends React.Component {
     }
   }
 
-  toggleValue(event, item) {
-    event.stopPropagation();
+  onRowClick({ rowData }) {
     const { model } = this.props;
-    model.selectListObjectValues('/qListObjectDef', [item.qElemNumber], true);
+    if (rowData) {
+      if (rowData[0].qState !== 'S') {
+        const rowDataToModify = rowData;
+        rowDataToModify[0].qState = 'S'; // For fast visual feedback, this will be overwritten when the new layout comes.
+      } else {
+        const rowDataToModify = rowData;
+        rowDataToModify[0].qState = 'O'; // For fast visual feedback, this will be overwritten when the new layout comes.
+      }
+      this.forceUpdate(); // Force a rerender to ge the previous changes to apear
+
+      model.selectListObjectValues('/qListObjectDef', [rowData[0].qElemNumber], true);
+    }
   }
 
   render() {
-    const { style } = this.state;
-    const { layout } = this.props;
+    const { layout, model } = this.props;
     if (!layout) {
       return null;
     }
+
     if (!layout.qListObject.qDataPages || layout.qListObject.qDataPages.length === 0) {
       return null;
     }
-    const items = layout.qListObject.qDataPages[0].qMatrix.map((matrixItem) => {
-      const item = matrixItem[0];
-      const classes = `item state-${item.qState}`;
-      return (
-        <li
-          key={item.qElemNumber}
-          className={classes}
-          onClick={event => this.toggleValue(event, item)}
-        >
-          {item.qText
-            + (item.qFrequency && item.qFrequency !== '1'
-              ? ` (${item.qFrequency}x)`
-              : '')}
-        </li>
-      );
-    });
-    const search = (
-      <input
-        autoFocus
-        onKeyUp={evt => this.search(evt)}
-        className="search"
-        placeholder="Search (wildcard)"
-      />
-    );
-    if (!items.length) {
-      return (
-        <div role="Listbox" tabIndex="-1" className="filterbox empty" style={style} onClick={preventDefaultFn}>
-          {search}
-          <ul className="items">
-            <li>No values.</li>
-          </ul>
-        </div>
-      );
-    }
+
     return (
-      <div role="Listbox" tabIndex="-1" className="filterbox" style={style} onClick={preventDefaultFn}>
-        {search}
-        <ul className="items">{items}</ul>
+      <div role="Listbox" tabIndex="-1" className="filterbox" onClick={preventDefaultFn}>
+        <input
+          onKeyUp={this.onSearch}
+          className="search"
+          placeholder="Search (wildcard)"
+        />
+        <div className="virtualtable">
+          <VirtualTable
+            layout={layout}
+            model={model}
+            onRowClick={this.onRowClick}
+            rowRenderer={rowRenderer}
+            noRowsRenderer={noRowsRenderer}
+          >
+            <Column
+              label="Name"
+              dataKey="name"
+              width={100}
+              flexGrow={1}
+              flexShrink={1}
+              cellDataGetter={listboxNameColumnGetter}
+              cellRenderer={nameCellRenderer}
+            />
+            <Column
+              width={50}
+              label="Description"
+              dataKey="description"
+              flexGrow={1}
+              flexShrink={0}
+              cellDataGetter={listboxFrequencyColumnGetter}
+              cellRenderer={freqCellRenderer}
+            />
+          </VirtualTable>
+        </div>
       </div>
     );
   }
@@ -117,16 +184,11 @@ export class Filterbox extends React.Component {
 Filterbox.defaultProps = {
   model: null,
   layout: null,
-  alignTo: null,
-  onClose: null,
 };
 
 Filterbox.propTypes = {
   model: PropTypes.object,
   layout: PropTypes.object,
-  field: PropTypes.string.isRequired, // eslint-disable-line react/no-unused-prop-types
-  alignTo: PropTypes.object,
-  onClose: PropTypes.func,
 };
 
-export default withApp(withModel({ WrappedComponent: withLayout(Filterbox), createModel: (app, props) => app.getOrCreateListbox(props.field, 20) }));
+export default Filterbox;
