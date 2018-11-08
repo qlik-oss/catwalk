14ae4d91dac22d34e090fcd46fbb0a04e2c3070f
Håkan Axelsson <han@qlik.com>
2018-11-08 15:24:33 +0100

    Update testcase

--- test/e2e/grid.spec.js
+++ test/e2e/grid.spec.js
@@ -1,15 +1,20 @@
 /* global page */
 // const puppeteer = require('puppeteer-core'); //eslint-disable-line
 
-const url = `http://host.docker.internal:1234/?engine_url=ws://qix-engine:9076/${process.env.DocID}`;
+const host = process.env.CI === 'true' ? 'localhost' : 'host.docker.internal';
+const engine = process.env.CI === 'true' ? 'localhost' : 'qix-engine';
+const app = process.env.DocID || 'drugcases.qvf';
+
+const url = `http://${host}:1234/?engine_url=ws://${engine}:9076/${app}`;
 const OPTS = {
   artifactsPath: 'test/e2e/__artifacts__/',
 };
 
 describe('Catwalk', () => {
   it('should render the grid correctly', async () => {
+    console.log(url);  //eslint-disable-line
     await page.goto(url, { timeout: 60000, waitUntil: 'networkidle0' });
-    // await page.waitFor(4000);
+    await page.waitFor(4000);
 
     const img = await page.screenshot({ fullPage: true });
     return expect(img).to.matchImageOf('grid', OPTS);
