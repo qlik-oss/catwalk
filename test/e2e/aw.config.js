f044f82eaf7d2fc856e6e47df64d70c39a1d1b2f
Håkan Axelsson <han@qlik.com>
2018-11-08 10:44:37 +0100

    Use external chrome

--- test/e2e/aw.config.js
+++ test/e2e/aw.config.js
@@ -1,3 +1,5 @@
 module.exports = {
   glob: ['test/e2e/*.spec.js'],
+  launch: false,
+  'chrome.browserWSEndpoint': 'ws://localhost:3000',
 };
--- test/e2e/grid.spec.js
+++ test/e2e/grid.spec.js
@@ -1,16 +1,15 @@
 /* global page */
+// const puppeteer = require('puppeteer-core'); //eslint-disable-line
 
-const url = `http://localhost:1234/?engine_url=ws://localhost:9076/${process.env.DocID}`;
+const url = `http://host.docker.internal:1234/?engine_url=ws://qix-engine:9076/${process.env.DocID}`;
 const OPTS = {
   artifactsPath: 'test/e2e/__artifacts__/',
 };
 
-
 describe('Catwalk', () => {
   it('should render the grid correctly', async () => {
-    console.log( await browser.version()); //eslint-disable-line
-    await page.goto(url, { timeout: 4000, waitUntil: 'networkidle0' });
-    await page.waitFor(4000);
+    await page.goto(url, { timeout: 60000, waitUntil: 'networkidle0' });
+    // await page.waitFor(4000);
 
     const img = await page.screenshot({ fullPage: true });
     return expect(img).to.matchImageOf('grid', OPTS);
