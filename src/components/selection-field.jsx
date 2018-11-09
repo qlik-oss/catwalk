8dc32d152ee39924a069ae3f98bcd8e94de65e8c
Helene Rignér <hvs@qlik.com>
2018-11-09 09:13:26 +0100

    Adding scrollbar when having many selection fields. (#85)
    Also closing the filterbox when clicking outside the filterbox area.

--- src/components/selection-field.jsx
+++ src/components/selection-field.jsx
@@ -1,64 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import PropTypes from 'prop-types';
 import Field from './field';
 import Filterbox from './filterbox';
 import useModel from './use/model';
 import useLayout from './use/layout';
+import useClickOutside from './use/click-outside';
 import './selection-field.pcss';
-
-function SelectionFieldWithoutState({
-  app, model, layout, field, fieldData,
-}) {
-  const [showFilterbox, setShowFilterbox] = useState(false);
-
-  const onClick = () => {
-    setShowFilterbox(!showFilterbox);
-  };
-
-  const onClearSelection = async (event) => {
-    event.stopPropagation();
-    if (app) {
-      const fieldObject = await app.getField(field);
-      fieldObject.clear();
-    }
-  };
-
-  const filterBox = showFilterbox
-    ? (
-      <div className="popover-content">
-        <Filterbox model={model} layout={layout} field={field} />
-      </div>
-    ) : null;
-
-  return (
-    <div className="popover-wrapper">
-      <div
-        className="selection-field"
-        onClick={onClick}
-        tabIndex="-1"
-        role="button"
-      >
-        <Field layout={layout} field={field} fieldData={fieldData} onClearSelection={onClearSelection} />
-      </div>
-      { filterBox }
-    </div>
-  );
-}
-
-SelectionFieldWithoutState.propTypes = {
-  app: PropTypes.object,
-  model: PropTypes.object,
-  layout: PropTypes.object,
-  field: PropTypes.string.isRequired,
-  fieldData: PropTypes.object,
-};
-
-SelectionFieldWithoutState.defaultProps = {
-  app: null,
-  model: null,
-  layout: null,
-  fieldData: null,
-};
 
 const createDefinition = field => ({
   qInfo: { qType: 'dmi-field' },
@@ -87,13 +34,67 @@ const createDefinition = field => ({
   },
 });
 
+
 export default function SelectionField({
   app, field, fieldData,
 }) {
+  const selfRef = useRef(null);
+  const [showFilterbox, setShowFilterbox] = useState(false);
   const model = useModel(app, createDefinition(field));
   const layout = useLayout(model);
 
-  return SelectionFieldWithoutState({
-    app, model, layout, field, fieldData,
+  const onClick = () => {
+    setShowFilterbox(!showFilterbox);
+  };
+
+  const onClearSelection = async (event) => {
+    event.stopPropagation();
+    if (app) {
+      const fieldObject = await app.getField(field);
+      fieldObject.clear();
+    }
+  };
+
+  useClickOutside(selfRef, showFilterbox, () => {
+    model.endSelections(true);
+    setShowFilterbox(false);
   });
+
+  let positioning = {};
+  if (selfRef && selfRef.current) {
+    positioning = { left: `${selfRef.current.getBoundingClientRect().left}px` };
+  }
+
+  const filterBox = showFilterbox
+    ? (
+      <div className="popover-content" style={positioning}>
+        <Filterbox model={model} layout={layout} field={field} />
+      </div>
+    ) : null;
+
+  return (
+    <div className="popover-wrapper" ref={selfRef}>
+      <div
+        className="selection-field"
+        onClick={onClick}
+        tabIndex="-1"
+        role="button"
+
+      >
+        <Field layout={layout} field={field} fieldData={fieldData} onClearSelection={onClearSelection} />
+      </div>
+      { filterBox }
+    </div>
+  );
 }
+
+SelectionField.propTypes = {
+  app: PropTypes.object,
+  field: PropTypes.string.isRequired,
+  fieldData: PropTypes.object,
+};
+
+SelectionField.defaultProps = {
+  app: null,
+  fieldData: null,
+};
