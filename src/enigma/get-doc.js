846a0eab21ab1a4d9ccc63903cb2cc4e3887f4ec
Helene Rigner <hvs@qlik.com>
2018-11-16 10:48:01 +0100

    Calling abortModal when fetching the app

--- src/enigma/get-doc.js
+++ src/enigma/get-doc.js
@@ -9,6 +9,8 @@ export default {
     async getDoc() {
       try {
         const activeDoc = await this.getActiveDoc();
+        // Make sure to end any selection state in the app that might have been set in the session before catwalk connected.
+        activeDoc.abortModal();
         return activeDoc;
       } catch (error) {
         if (error.code) {
