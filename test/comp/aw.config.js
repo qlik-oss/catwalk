c1227a7a2747cadb6c1a7f1cba1b73b69d5b6d3a
Håkan Axelsson <han@qlik.com>
2018-11-22 05:15:20 -0500

    Add aw (#98)
    * Adding test structure

---
+++ test/comp/aw.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  glob: 'test/comp/**/*.spec.jsx',
+  // coverage: true,
+  src: ['src/components/**/*.jsx'],
+  mocks: [
+    ['**/*.{svg,css,scss,pcss}'],
+  ],
+};
---
+++ test/e2e/grid.spec.js
@@ -0,0 +1,18 @@
+const host = process.env.CI === 'true' ? 'localhost' : 'host.docker.internal';
+const engine = process.env.CI === 'true' ? 'localhost' : 'qix-engine';
+const app = process.env.DocID || 'drugcases.qvf';
+
+const url = `http://${host}:1234/?engine_url=ws://${engine}:9076/${app}`;
+const OPTS = {
+  artifactsPath: 'test/e2e/__artifacts__/',
+};
+
+describe('catwalk', () => {
+  it('should render the grid correctly', async () => {
+    await page.goto(url, { timeout: 60000, waitUntil: 'networkidle0' });
+    await page.waitFor(4000);
+
+    const img = await page.screenshot({ fullPage: true });
+    return expect(img).to.matchImageOf('grid', OPTS);
+  });
+});
