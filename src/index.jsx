18cf008629ce72dec64adcf1938c6aa3a65fc0b4
Håkan Axelsson <han@qlik.com>
2018-11-27 10:39:38 +0100

    add listners

--- src/index.jsx
+++ src/index.jsx
@@ -10,6 +10,20 @@ import './index.pcss';
 if (process.env.GA) {
   ReactGA.initialize(process.env.GA);
   ReactGA.pageview(window.location.pathname + window.location.search);
+
+  window.onerror = (message, source, lineno, colno, error) => {
+    console.log('ERROR REPORTING', message, source, lineno, colno, error);
+    // ReactGA.exception({
+    //   description: error,
+    // });
+  };
+
+  window.onunhandledrejection = (error) => {
+    console.log('UNHANDLED REJECTION REPORTING', error, error.reason);
+    // ReactGA.exception({
+    //   description: error,
+    // });
+  };
 }
 
 
