aa3c28b7204471c8ed1eb15180ec41b55e71fac3
Håkan Axelsson <han@qlik.com>
2018-11-29 14:01:07 +0100

    Add cookie consent and change to eventListners

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactGA from 'react-ga';
+import CookieConsent from 'react-cookie-consent';
 
 import ErrorBoundary from './components/error-boundary';
 import App from './components/app';
@@ -11,25 +12,32 @@ if (process.env.GA) {
   ReactGA.initialize(process.env.GA);
   ReactGA.pageview(window.location.pathname + window.location.search);
 
-  window.onerror = (message, source, lineno, colno, error) => {
-    console.log('ERROR REPORTING', message, source, lineno, colno, error);
+  window.addEventListener('error', (event) => {
     ReactGA.exception({
-      description: JSON.stringify(error),
+      description: `ERROR: ${JSON.stringify(event, Object.getOwnPropertyNames(event))}`,
     });
-  };
+  });
 
-  window.onunhandledrejection = (error) => {
-    console.log('UNHANDLED REJECTION REPORTING', error, error.reason);
+  window.addEventListener('unhandledrejection', (event) => {
     ReactGA.exception({
-      description: JSON.stringify(error.reason),
+      description: `UNHANDLED REJECTION: ${JSON.stringify(event.reason, Object.getOwnPropertyNames(event.reason))}`,
     });
-  };
+  });
 }
 
 
 const Index = () => (
   <ErrorBoundary>
     <App />
+    <CookieConsent
+      buttonText="Got it!"
+      style={{ background: '#5F6062', color: '#D6D6D6', fontSize: '2rem' }}
+      buttonStyle={{ color: '#fff', background: '#3D8706', fontSize: '2rem' }}
+    >
+      This website uses cookies to ensure you get the best experience on our website.
+      {' '}
+      <a href="https://www.qlik.com/us/legal/cookies-and-privacy-policy" target="_blank" rel="noopener noreferrer" style={{ color: '#D6D6D6' }}>Learn more</a>
+    </CookieConsent>
   </ErrorBoundary>
 );
 
