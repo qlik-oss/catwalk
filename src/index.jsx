9346e44f269b8aa6628e4abbef690abf7ade367d
Håkan Axelsson <han@qlik.com>
2018-11-29 16:28:59 +0100

    Restructure

--- src/index.jsx
+++ src/index.jsx
@@ -8,36 +8,43 @@ import App from './components/app';
 
 import './index.pcss';
 
-if (process.env.GA) {
-  ReactGA.initialize(process.env.GA);
-  ReactGA.pageview(window.location.pathname + window.location.search);
+const errorReporting = () => {
+  if (process.env.GA) {
+    ReactGA.initialize(process.env.GA);
+    ReactGA.pageview(window.location.pathname + window.location.search);
 
-  window.addEventListener('error', (event) => {
-    ReactGA.exception({
-      description: `ERROR: ${JSON.stringify(event, Object.getOwnPropertyNames(event))}`,
+    window.addEventListener('error', (event) => {
+      ReactGA.exception({
+        description: `ERROR: ${JSON.stringify(event, Object.getOwnPropertyNames(event))}`,
+      });
     });
-  });
 
-  window.addEventListener('unhandledrejection', (event) => {
-    ReactGA.exception({
-      description: `UNHANDLED REJECTION: ${JSON.stringify(event.reason, Object.getOwnPropertyNames(event.reason))}`,
+    window.addEventListener('unhandledrejection', (event) => {
+      ReactGA.exception({
+        description: `UNHANDLED REJECTION: ${JSON.stringify(event.reason, Object.getOwnPropertyNames(event.reason))}`,
+      });
     });
-  });
-}
+
+    return (
+      <CookieConsent
+        buttonText="Got it!"
+        style={{ background: '#5F6062', color: '#D6D6D6', fontSize: '2rem' }}
+        buttonStyle={{ color: '#fff', background: '#3D8706', fontSize: '2rem' }}
+      >
+        This website uses cookies to ensure you get the best experience on our website.
+        {' '}
+        <a href="https://www.qlik.com/us/legal/cookies-and-privacy-policy" target="_blank" rel="noopener noreferrer" style={{ color: '#D6D6D6' }}>Learn more</a>
+      </CookieConsent>
+    );
+  }
+  return null;
+};
 
 
 const Index = () => (
   <ErrorBoundary>
     <App />
-    <CookieConsent
-      buttonText="Got it!"
-      style={{ background: '#5F6062', color: '#D6D6D6', fontSize: '2rem' }}
-      buttonStyle={{ color: '#fff', background: '#3D8706', fontSize: '2rem' }}
-    >
-      This website uses cookies to ensure you get the best experience on our website.
-      {' '}
-      <a href="https://www.qlik.com/us/legal/cookies-and-privacy-policy" target="_blank" rel="noopener noreferrer" style={{ color: '#D6D6D6' }}>Learn more</a>
-    </CookieConsent>
+    {errorReporting()}
   </ErrorBoundary>
 );
 
