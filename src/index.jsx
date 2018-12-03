6c4b6765ca9bf62ac6e2d52b9a8cd360b921d928
Håkan Axelsson <han@qlik.com>
2018-12-03 13:06:46 +0100

    add error-reporting

--- src/index.jsx
+++ src/index.jsx
@@ -1,62 +1,17 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import ReactGA from 'react-ga';
-import CookieConsent from 'react-cookie-consent';
+
 
 import ErrorBoundary from './components/error-boundary';
 import App from './components/app';
+import UseErrorReporting from './components/error-reporting';
 
 import './index.pcss';
-
-const sendException = (event) => {
-  let message;
-
-  if (event.type === 'unhandledrejection') {
-    message = `UNHANDLED REJECTION: ${JSON.stringify(event.reason, Object.getOwnPropertyNames(event.reason))}`;
-  } else {
-    message = `ERROR: ${JSON.stringify(event, Object.getOwnPropertyNames(event))}`;
-  }
-
-  ReactGA.exception({
-    description: message,
-  });
-};
-
-const errorReporting = () => {
-  if (process.env.GA) {
-    ReactGA.initialize(process.env.GA);
-    ReactGA.pageview(window.location.pathname + window.location.search);
-
-    useEffect(() => {
-      window.addEventListener('error', sendException);
-      window.addEventListener('unhandledrejection', sendException);
-
-      return () => {
-        window.removeEventListener('error', sendException);
-        window.removeEventListener('unhandledrejection', sendException);
-      };
-    }, []);
-
-    return (
-      <CookieConsent
-        buttonText="Got it!"
-        style={{ background: '#5F6062', color: '#D6D6D6', fontSize: '2rem' }}
-        buttonStyle={{ color: '#fff', background: '#3D8706', fontSize: '2rem' }}
-      >
-        This website uses cookies to ensure you get the best experience on our website.
-        {' '}
-        <a href="https://www.qlik.com/us/legal/cookies-and-privacy-policy" target="_blank" rel="noopener noreferrer" style={{ color: '#D6D6D6' }}>Learn more</a>
-      </CookieConsent>
-    );
-  }
-  return null;
-};
-
 
 const Index = () => (
   <ErrorBoundary>
     <App />
-    {errorReporting()}
+    <UseErrorReporting GA={process.env.GA} />
   </ErrorBoundary>
 );
 
