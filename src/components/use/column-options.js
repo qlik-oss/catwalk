5bd4bdad10893c0e8e91c6c92c755e63e1f683af
Helene Rigner <hvs@qlik.com>
2018-12-06 13:49:44 +0100

    Using qLabel and qFieldLabels as measure/dim title if no metadef is available

--- src/components/use/column-options.js
+++ src/components/use/column-options.js
@@ -12,7 +12,7 @@ const genericObjectProperties = {
     qType: 'measure',
     qData: {
       title: '/qMetaDef/title',
-      expression: '/qMeasure/qDef',
+      label: '/qMeasure/qLabel',
     },
   },
   qFieldListDef: {
@@ -23,26 +23,45 @@ const genericObjectProperties = {
     qData: {
       title: '/qMetaDef/title',
       all: '/qDim',
-      tags: '/qMetaDef/tags',
-      grouping: '/qDim/qGrouping',
-      info: '/qDimInfos',
-      labelExpression: '/qDim/qLabelExpression',
     },
   },
 };
 
 export function layoutToListsStructure(layout) {
   if (layout) {
-    const measures = layout.qMeasureList.qItems ? layout.qMeasureList.qItems.map(measure => ({
-      title: measure.qData.title ? measure.qData.title : measure.qInfo.qId,
-      type: 'measure',
-      hyperCubeContent: { qLibraryId: measure.qInfo.qId },
-    })) : [];
-    const dimensions = layout.qDimensionList.qItems ? layout.qDimensionList.qItems.map(dimension => ({
-      title: dimension.qData.title ? dimension.qData.title : dimension.qInfo.qId,
-      type: 'dimension',
-      hyperCubeContent: { qLibraryId: dimension.qInfo.qId },
-    })) : [];
+    const measures = layout.qMeasureList.qItems ? layout.qMeasureList.qItems.map((measure) => {
+      let measureTitle = measure.qInfo.qId;
+      if (measure.qData.title) {
+        const { title } = measure.qData;
+        measureTitle = title;
+      } else if (measure.qData.label) {
+        measureTitle = measure.qData.label;
+      }
+      return (
+        {
+          title: measureTitle,
+          type: 'measure',
+          hyperCubeContent: { qLibraryId: measure.qInfo.qId },
+        }
+      );
+    }) : [];
+    const dimensions = layout.qDimensionList.qItems ? layout.qDimensionList.qItems.map((dimension) => {
+      let dimTitle = dimension.qInfo.qId;
+      if (dimension.qData.title) {
+        const { title } = dimension.qData;
+        dimTitle = title;
+      } else if (dimension.qData.all.qFieldLabels && dimension.qData.all.qFieldLabels.length > 0) {
+        const [label] = dimension.qData.all.qFieldLabels;
+        dimTitle = label;
+      }
+      return (
+        {
+          title: dimTitle,
+          type: 'dimension',
+          hyperCubeContent: { qLibraryId: dimension.qInfo.qId },
+        }
+      );
+    }) : [];
     const fields = layout.qFieldList.qItems ? layout.qFieldList.qItems.map(field => ({ title: field.qName, type: 'field', hyperCubeContent: { qDef: { qFieldDefs: [field.qName] } } })) : [];
 
     const result = [
