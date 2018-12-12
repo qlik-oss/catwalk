a036a021244f67fbda2181ff18f38bf106562a82
Håkan Axelsson <han@qlik.com>
2018-12-12 09:17:13 +0100

    Adding code coverage

--- test/comp/aw.config.js
+++ test/comp/aw.config.js
@@ -1,6 +1,13 @@
+// const { JSDOM } = require('jsdom');
+
+// global.document = new JSDOM().window;
+
+global.document = { location: { search: '' } };
+global.document.createElement = () => '';
+
 module.exports = {
   glob: 'test/comp/**/*.spec.jsx',
-  // coverage: true,
+  coverage: true,
   src: ['src/components/**/*.jsx'],
   mocks: [
     ['**/*.{svg,css,scss,pcss}'],
