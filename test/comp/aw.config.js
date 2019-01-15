b965bd81719b7ddc138e3b0f1ad2c6e4fb3a216a
Håkan Axelsson <han@qlik.com>
2019-01-15 09:59:58 +0100

    More comp test (#137)
    * Adding code coverage
    
    * fix lint
    
    * Adding mock for browser API and fix for SVG in Firefox
    
    * Add npm script to create html coverage report
    
    * removed commented out code
    
    * locked dependencies

--- test/comp/aw.config.js
+++ test/comp/aw.config.js
@@ -1,8 +1,42 @@
+const { JSDOM } = require('jsdom');
+
+const template = `
+<html>
+<head>
+</head>
+<body>
+</body>
+</html>
+`;
+
+function copyProps(src, target) {
+  const props = Object.getOwnPropertyNames(src)
+    .filter(prop => typeof target[prop] === 'undefined')
+    .reduce((result, prop) => ({
+      ...result,
+      [prop]: Object.getOwnPropertyDescriptor(src, prop),
+    }), {});
+  Object.defineProperties(target, props);
+}
+
+const jsdom = new JSDOM(template);
+const { window } = jsdom;
+global.window = window;
+global.document = window.document;
+global.navigator = {
+  userAgent: 'node.js',
+};
+global.window.cancelAnimationFrame = () => {};
+global.window.requestAnimationFrame = () => {};
+copyProps(window, global);
+
+
 module.exports = {
   glob: 'test/comp/**/*.spec.jsx',
-  // coverage: true,
+  coverage: true,
   src: ['src/components/**/*.jsx'],
   mocks: [
     ['**/*.{svg,css,scss,pcss}'],
   ],
+  exit: true,
 };
