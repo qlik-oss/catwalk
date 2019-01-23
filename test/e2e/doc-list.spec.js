53e926822b6c54d4f85ce7030424a909e33886be
Fredrik Folkesson <Fredrik.Folkesson@qlik.com>
2019-01-23 17:31:20 +0100

    speed up animation in both test cases

--- test/e2e/doc-list.spec.js
+++ test/e2e/doc-list.spec.js
@@ -6,10 +6,15 @@ const correctEngineUrl = `http://${host}:1234/?engine_url=ws://${engine}:9076/`;
 const OPTS = {
   artifactsPath: 'test/e2e/__artifacts__/',
 };
+let page;
 
 describe('doc-list', () => {
+  beforeEach(async () => {
+    page = await browser.newPage();
+    await page._client.send('Animation.setPlaybackRate', { playbackRate: 12 });
+  });
+
   it('should show the no engine found with invalid engine url', async () => {
-    const page = await browser.newPage();
     await page.goto(incorrectEngineUrl, { timeout: 60000, waitUntil: 'networkidle0' });
     await page.waitForSelector('[value=Connect]');
     const img = await page.screenshot({ fullPage: true });
@@ -18,8 +23,6 @@ describe('doc-list', () => {
 
   it('should show the doc list when a valid engine url is provided', async () => {
     // Check that the doc-list is rendered correctly
-    const page = await browser.newPage();
-    await page._client.send('Animation.setPlaybackRate', { playbackRate: 12 });
     await page.goto(correctEngineUrl, { timeout: 60000, waitUntil: 'networkidle0' });
     await page.waitForSelector('.doc-list');
     let img = await page.screenshot({ fullPage: true });
