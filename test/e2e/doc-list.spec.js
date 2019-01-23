2aab3e3d0d06f86a216c56f43015f048a1349dfa
Fredrik Folkesson <Fredrik.Folkesson@qlik.com>
2019-01-23 18:28:30 +0100

    wait for other elements

--- test/e2e/doc-list.spec.js
+++ test/e2e/doc-list.spec.js
@@ -15,7 +15,7 @@ describe('doc-list', () => {
   });
 
   it('should show the no engine found with invalid engine url', async () => {
-    await page.goto(incorrectEngineUrl, { timeout: 60000, waitUntil: ['networkidle0', 'domcontentloaded', 'load'] });
+    await page.goto(incorrectEngineUrl, { timeout: 60000, waitUntil: 'networkidle0' });
     await page.waitForSelector('[value=Connect]');
     const img = await page.screenshot({ fullPage: true });
     await expect(img).to.matchImageOf('no-engine-doc-list', OPTS);
@@ -23,14 +23,14 @@ describe('doc-list', () => {
 
   it('should show the doc list when a valid engine url is provided', async () => {
     // Check that the doc-list is rendered correctly
-    await page.goto(correctEngineUrl, { timeout: 60000, waitUntil: ['networkidle0', 'domcontentloaded', 'load'] });
+    await page.goto(correctEngineUrl, { timeout: 60000, waitUntil: 'networkidle0' });
     await page.waitForSelector('.doc-list');
     let img = await page.screenshot({ fullPage: true });
     await expect(img).to.matchImageOf('valid-engine-doc-list', OPTS);
 
     // Verify that clicking on an app opens it.
     await page.click('.doc-list > li');
-    await page.waitForSelector('[tablez=Drug]');
+    await page.waitForSelector('.name-and-text > .bartext');
     img = await page.screenshot({ fullPage: true });
     await expect(img).to.matchImageOf('loaded-app', OPTS);
   });
