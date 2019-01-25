f537ef3967f98faf10dd22f7bf2600d83d0792b1
Fredrik Folkesson <Fredrik.Folkesson@qlik.com>
2019-01-25 09:33:50 +0100

    Test for most of the views of basic usage flow (#148)
    To be merged after the doc-list one. (or instead of it)
    
    Tests making selections, making a selection inside the selection bar, opening the hypercube view, creating a hypercube and opening the top right menu.

--- test/e2e/aw.config.local.js
+++ test/e2e/aw.config.local.js
@@ -6,4 +6,5 @@ module.exports = {
     width: 1920,
     height: 1080,
   },
+  'mocha.bail': false,
 };
---
+++ test/e2e/doc-list.spec.js
@@ -0,0 +1,46 @@
+const wsHelper = require('./test-helper');
+
+const host = process.env.CI === 'true' ? 'localhost' : 'host.docker.internal';
+const engine = process.env.CI === 'true' ? 'localhost' : 'qix-engine';
+
+const incorrectEngineUrl = `http://${host}:1234/?engine_url=ws://incorrect-url:9076`;
+const correctEngineUrl = `http://${host}:1234/?engine_url=ws://${engine}:9076/`;
+const OPTS = {
+  artifactsPath: 'test/e2e/__artifacts__/',
+};
+let page;
+
+describe('doc-list', () => {
+  beforeEach(async () => {
+    page = await browser.newPage();
+    const client = page._client;
+    await client.send('Animation.setPlaybackRate', { playbackRate: 1000 });
+
+    wsHelper.init(client);
+  });
+
+  it('should show the no engine found with invalid engine url', async () => {
+    await page.goto(incorrectEngineUrl, { timeout: 60000, waitUntil: 'networkidle0' });
+    await page.waitForSelector('[value=Connect]');
+    const img = await page.screenshot({ fullPage: true });
+    await expect(img).to.matchImageOf('no-engine-doc-list', OPTS);
+  });
+
+  it('should show the doc list when a valid engine url is provided', async () => {
+    // Check that the doc-list is rendered correctly.
+    await page.goto(correctEngineUrl, { timeout: 60000, waitUntil: 'networkidle0' });
+    await page.waitForSelector('.doc-list');
+    let img = await page.screenshot({ fullPage: true });
+    await expect(img).to.matchImageOf('valid-engine-doc-list', OPTS);
+
+    // Verify that clicking on an app opens it.
+    page.click('.doc-list > li');
+
+    await page.waitForSelector('.model');
+    await wsHelper.waitUntilNoRequests(500);
+
+
+    img = await page.screenshot({ fullPage: true });
+    await expect(img).to.matchImageOf('loaded-app', OPTS);
+  });
+});
