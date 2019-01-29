ddb4cfe53ae49f1b2e219429a8352fd66199e588
Håkan Axelsson <han@qlik.com>
2019-01-29 16:10:03 +0100

    Adding error handling to the test setup (#156)

--- test/e2e/_setup.spec.js
+++ test/e2e/_setup.spec.js
@@ -1,8 +1,11 @@
 /* global before, after */
+/* eslint no-plusplus: ["error", { "allowForLoopAfterthoughts": true }] */
 
 const fs = require('fs-extra');
 const NYC = require('nyc');
 const wsHelper = require('./test-helper');
+
+let coverageFlag = false;
 
 before(async () => {
   await fs.emptyDir('.nyc_output');
@@ -33,11 +36,37 @@ beforeEach(async () => {
 /* eslint-disable-next-line */
 afterEach(async function () {
   const coverage = await page.evaluate(() => window.__coverage__);
-  return fs.writeJson(`.nyc_output/${new Date().getTime()}.json`, coverage);
+  if (coverage) {
+    coverageFlag = true;
+    await fs.writeJson(`.nyc_output/${new Date().getTime()}.json`, coverage);
+  }
+
+  const ignoreAsserts = (message) => {
+    const errors2skip = ['No baseline found!', 'equality to be less than'];
+    let found = false;
+
+    for (let index = 0; index < errors2skip.length; index++) {
+      if (message.includes(errors2skip[index])) {
+        found = true;
+        break;
+      }
+    }
+
+    return found;
+  };
+
+  if (this.currentTest.state === 'failed' && !ignoreAsserts(this.currentTest.err.message)) {
+    const fullname = await this.currentTest.fullTitle();
+    await page.screenshot({ fullPage: true, path: `${OPTS.artifactsPath}${fullname}.png` });
+  }
 });
 
 after(async () => {
   const nyc = new NYC();
   await nyc.report();
+  if (!coverageFlag) {
+    console.log('\u001b[31m The code hasn´t been instrumented! Please start the server with `npm run start:cov`\u001b[0m'); // eslint-disable-line no-console
+  }
+
   await browser.close();
 });
