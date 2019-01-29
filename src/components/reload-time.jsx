81e9c825bc5c699c76a0f78f372618d153dedb0b
Helene Rigner <hvs@qlik.com>
2019-01-29 12:22:23 +0100

    Fixing problem with disapperaring menu

---
+++ src/components/reload-time.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+
+export default function ReloadTime({ lastReloadTime, className }) {
+  const [lastReloadString, setLastReloadString] = useState('');
+  const [lastRefresh, setLastRefresh] = useState(null);
+  const [refreshTimer, setRefreshTimer] = useState(0);
+
+  useEffect(() => {
+    const now = new Date();
+    const lastReloaded = new Date(lastReloadTime);
+    const secondsSince = (now.getTime() - lastReloaded.getTime()) / 1000;
+    const minutesSince = secondsSince / 60;
+    const hoursSince = secondsSince / 3600;
+    const interval = 5 * 1000;
+    if (secondsSince < 60) {
+      setLastReloadString('App reloaded less than a minute ago.');
+    } else if (hoursSince < 1) {
+      setLastReloadString(`App reloaded ${Math.floor(minutesSince)} ${minutesSince <= 2 ? 'minute' : 'minutes'} ago`);
+    } else {
+      setLastReloadString(`App reloaded at ${lastReloaded.toUTCString()}`);
+    }
+    setRefreshTimer(setTimeout(() => { setLastRefresh(now); }, interval));
+    return () => clearTimeout(refreshTimer);
+  }, [lastRefresh]);
+  return (
+    <div className={className}>
+      {lastReloadString}
+    </div>
+  );
+}
+
+ReloadTime.propTypes = {
+  lastReloadTime: PropTypes.string.isRequired,
+  className: PropTypes.string.isRequired,
+};
--- src/components/topbar.jsx
+++ src/components/topbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import SVGInline from 'react-svg-inline';
 import {
@@ -10,6 +10,7 @@ import {
 } from 'react-contexify';
 
 import Selections from './selections';
+import ReloadTime from './reload-time';
 import logo from '../assets/catwalk.svg';
 import moreHorizontalOutline from '../assets/more-horizontal-outline.svg';
 
@@ -17,28 +18,6 @@ import 'react-contexify/dist/ReactContexify.min.css';
 import './topbar.pcss';
 
 export default function TopBar({ app, appLayout: { qLastReloadTime }, startGuide }) {
-  const [lastReloadString, setLastReloadString] = useState('');
-  const [lastRefresh, setLastRefresh] = useState(null);
-  const [refreshTimer, setRefreshTimer] = useState(0);
-
-  useEffect(() => {
-    const now = new Date();
-    const lastReloaded = new Date(qLastReloadTime);
-    const secondsSince = (now.getTime() - lastReloaded.getTime()) / 1000;
-    const minutesSince = secondsSince / 60;
-    const hoursSince = secondsSince / 3600;
-    const interval = 5 * 1000;
-    if (secondsSince < 60) {
-      setLastReloadString('App reloaded less than a minute ago.');
-    } else if (hoursSince < 1) {
-      setLastReloadString(`App reloaded ${Math.floor(minutesSince)} ${minutesSince <= 2 ? 'minute' : 'minutes'} ago`);
-    } else {
-      setLastReloadString(`App reloaded at ${lastReloaded.toUTCString()}`);
-    }
-    setRefreshTimer(setTimeout(() => setLastRefresh(now), interval));
-    return () => clearTimeout(refreshTimer);
-  }, [lastRefresh]);
-
   const chooseApp = () => {
     const URLobject = new URL(window.location.href);
     window.location.assign(`${URLobject.protocol}//${window.location.host}`);
@@ -63,9 +42,7 @@ export default function TopBar({ app, appLayout: { qLastReloadTime }, startGuide
         <SVGInline className="logo" svg={logo} />
       </div>
       <Selections app={app} />
-      <div className="reloaded">
-        {lastReloadString}
-      </div>
+      <ReloadTime lastReloadTime={qLastReloadTime} className="reloaded" />
       <MenuProvider id="menu_id" event="onClick" className="menu-provider">
         <div>
           <SVGInline className="more-icon" svg={moreHorizontalOutline} />
