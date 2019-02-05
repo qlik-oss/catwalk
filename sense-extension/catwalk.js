c51ab347d63251edb7cbcf65c970a3563669c6bc
Håkan Axelsson <han@qlik.com>
2019-02-05 10:40:59 +0100

    Fix lint in sense extension

--- sense-extension/catwalk.js
+++ sense-extension/catwalk.js
@@ -1,28 +1,24 @@
-define( [ 'qlik', 'jquery'
-],
-function (qlik, $) {
-	return {
-		support: {
-			snapshot: false,
-			export: false,
-			exportData: false
-		},
-		paint: function ($element) {
-			qlik.currApp().getAppLayout().then(applayout => {
-				const wsString = applayout.session.cacheName;
-				const cleandWS = wsString.substring( 0, wsString.indexOf('?'));
-				$element.empty();
-				$element.append(`
-					<a target="_blank" rel="noopener noreferrer" href="https://catwalk.core.qlik.com/?engine_url=${cleandWS}">
-						<img src="/extensions/catwalk/catwalk.svg" />
-					</a>
-				`);
+/* globals define */
+/* eslint-disable import/no-amd */
 
-
-    		});
-
-			return qlik.Promise.resolve();
-		}
-	};
-} );
-
+define(['qlik'],
+  qlik => ({
+    support: {
+      snapshot: false,
+      export: false,
+      exportData: false,
+    },
+    paint($element) {
+      qlik.currApp().getAppLayout().then((applayout) => {
+        const wsString = applayout.session.cacheName;
+        const cleandWS = wsString.substring(0, wsString.indexOf('?'));
+        $element.empty();
+        $element.append(`
+          <a target="_blank" rel="noopener noreferrer" href="https://catwalk.core.qlik.com/?engine_url=${cleandWS}">
+            <img src="/extensions/catwalk/catwalk.svg" />
+          </a>
+        `);
+      });
+      return qlik.Promise.resolve();
+    },
+  }));
