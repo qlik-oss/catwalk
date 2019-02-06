4e86de463766c15ebb9d93e5eef603298e23d86d
Karl Fredberg Sjöstrand <karl.fredberg@gmail.com>
2019-02-06 14:46:08 +0100

    Update port logic
    Works for desktop, server and with docker container
    Add fallback if appId is not present in url, go to catwalk.core.qlik.com

--- bookmark/bookmark.js
+++ bookmark/bookmark.js
@@ -1,16 +1,24 @@
 javascript: (function () {
-  const { hostname } = window.location;
-
-  const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
   const getAppId = (location) => {
     const prefix = '/sense/app/';
-    const start = location.pathname.indexOf(prefix) + prefix.length;
+    let start = location.pathname.indexOf(prefix);
+    if (start === -1) return;
+    start += prefix.length;
     const end = location.pathname.indexOf('/', start);
     const appId = location.pathname.substring(start, end !== -1 ? end : location.pathname.length);
     return appId;
   };
   const appId = getAppId(window.location);
-  const engineUrl = `${protocol}://${hostname}:9076/app/${appId}`;
-  const url = `http://catwalk.core.qlik.com/?engine_url=${engineUrl}`;
-  window.open(url, '_blank');
+  if (appId) {
+    const { hostname } = window.location;
+    const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+    const port = window.location.port === '' ? '': `:${window.location.port}`;
+
+    const engineUrl = `${protocol}://${hostname}${port}/app/${appId}`;
+    const url = `http://catwalk.core.qlik.com/?engine_url=${engineUrl}`;
+    window.open(url, '_blank');
+  } else {
+    const url = 'http://catwalk.core.qlik.com';
+    window.open(url, '_blank');
+  }
 }());
