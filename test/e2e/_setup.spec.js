775723c983e1ef0495c16168201d7ff0ee00bcd7
Håkan Axelsson <han@qlik.com>
2020-01-02 11:55:04 +0100

    Fix for breaking change in NYC

--- test/e2e/_setup.spec.js
+++ test/e2e/_setup.spec.js
@@ -62,8 +62,8 @@ afterEach(async function () {
 });
 
 after(async () => {
-  const nyc = new NYC();
-  await nyc.report();
+  // const nyc = new NYC();
+  // await nyc.report();
   if (!coverageFlag) {
     console.log('\u001b[31m The code hasn´t been instrumented! Please start the server with `npm run start:cov`\u001b[0m'); // eslint-disable-line no-console
   }
