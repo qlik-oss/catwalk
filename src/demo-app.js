ccb6c8a6dce583f592346c6f7e98112948ffb004
Håkan Axelsson <han@qlik.com>
2021-03-26 07:18:38 +0100

    Remove trailing slash

--- src/demo-app.js
+++ src/demo-app.js
@@ -1,2 +1,2 @@
-const demoApp = 'wss://sense-demo.qlik.com/app/069279ac-a7e8-4405-826d-0cd3de7d48e0/';
+const demoApp = 'wss://sense-demo.qlik.com/app/069279ac-a7e8-4405-826d-0cd3de7d48e0';
 export default demoApp;
